42ed15098521b7ddb2cfdae70fd07e11b868ae06
Lewis Tian <taseikyo@gmail.com>
2019-12-30 20:38:40 +0800

    remove error links

--- google-translate/main.py
+++ google-translate/main.py
@@ -6,23 +6,24 @@
 # @Version : Python3.6
 
 from mwin import Ui_MWin
- 
+
 from PyQt5.QtCore import QTranslator, QUrl, QThread, pyqtSignal, Qt
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QFileDialog, QMessageBox
-from  PyQt5.QtGui import QDesktopServices, QPixmap
+from PyQt5.QtGui import QDesktopServices, QPixmap
 import sys
 from googletrans import Translator
 import re
 
-GTransData = ''
+GTransData = ""
+
 
 class MyWindow(QMainWindow, Ui_MWin):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
         self.setupUi(self)
         try:
-            with open('style.qss') as f: 
-                style = f.read() # 读取样式表
+            with open("style.qss") as f:
+                style = f.read()  # 读取样式表
                 self.setStyleSheet(style)
         except:
             print("open stylesheet error")
@@ -30,17 +31,19 @@ class MyWindow(QMainWindow, Ui_MWin):
         #  Translator
         self.trans = QTranslator()
         # destination language
-        self.dest = 'zh-CN'
+        self.dest = "zh-CN"
         # ui language : 0->zh-CN, 1->en
-        self.lan = 0 
+        self.lan = 0
         # real-time translate
         self.isRealTimeTrans = False
         self.isCopyFromTrans = False
         self.connectSlots()
 
     def connectSlots(self):
-        self.alwaysFront.clicked.connect(self.alwaysFrontFunc) # windows top
-        self.realTimeTrans.clicked.connect(self.realTimeTransFunc) # real-time translate
+        self.alwaysFront.clicked.connect(self.alwaysFrontFunc)  # windows top
+        self.realTimeTrans.clicked.connect(
+            self.realTimeTransFunc
+        )  # real-time translate
 
         self.go.clicked.connect(self.transTextToZhCN)
         self.go.setShortcut("CTRL+Return")
@@ -60,16 +63,30 @@ class MyWindow(QMainWindow, Ui_MWin):
         self.actionDestJapanese.triggered.connect(lambda: self.destinationLanguage(2))
         self.actionDestKorean.triggered.connect(lambda: self.destinationLanguage(3))
 
-        self.about.triggered.connect(lambda: QDesktopServices.openUrl(QUrl('https://github.com/LewisTian/PyQt5-Apps/tree/PyQt5-Apps/google-translate')))
+        self.about.triggered.connect(
+            lambda: QDesktopServices.openUrl(
+                QUrl(
+                    "https://github.com/taseikyo/PyQt5-Apps/tree/master/google-translate"
+                )
+            )
+        )
         self.about.setShortcut("CTRL+H")
-        self.donate.triggered.connect(lambda: QDesktopServices.openUrl(QUrl('https://lewistian.github.io/2018/01/01/donate/')))
-        self.reportBug.triggered.connect(lambda: QDesktopServices.openUrl(QUrl('https://github.com/LewisTian/PyQt5-Apps/issues')))
+        self.donate.triggered.connect(
+            lambda: QDesktopServices.openUrl(
+                QUrl("https://github.com/taseikyo/PyQt5-Apps#donation")
+            )
+        )
+        self.reportBug.triggered.connect(
+            lambda: QDesktopServices.openUrl(
+                QUrl("https://github.com/taseikyo/PyQt5-Apps/issues")
+            )
+        )
 
     def openFileSlot(self):
-        filename, filetype = QFileDialog.getOpenFileName(self, 'Open File', '.')
+        filename, filetype = QFileDialog.getOpenFileName(self, "Open File", ".")
         if filename:
             print(filename)
-            with open(filename, encoding = 'utf-8') as f:
+            with open(filename, encoding="utf-8") as f:
                 try:
                     self.originText.setPlainText(str(f.read()))
                 except Exception as e:
@@ -78,9 +95,11 @@ class MyWindow(QMainWindow, Ui_MWin):
     def exportFileSlot(self):
         if not self.transText.toPlainText():
             return
-        filename, filetype = QFileDialog.getSaveFileName(self, 'Save File', '.', '*.txt;;*')
+        filename, filetype = QFileDialog.getSaveFileName(
+            self, "Save File", ".", "*.txt;;*"
+        )
         if filename:
-            with open(filename, 'w', encoding = 'utf-8') as f:
+            with open(filename, "w", encoding="utf-8") as f:
                 try:
                     f.write(self.transText.toPlainText())
                 except Exception as e:
@@ -96,7 +115,7 @@ class MyWindow(QMainWindow, Ui_MWin):
             print("[MainWindow] Change to zh_CN")
             self.trans.load("zh_CN")
         elif lan == 1 and self.lan != 1:
-            self.lan =1
+            self.lan = 1
             print("[MainWindow] Change to English")
             self.trans.load("en")
         else:
@@ -111,29 +130,40 @@ class MyWindow(QMainWindow, Ui_MWin):
         change destination language
         """
         if lan == 0:
-            self.dest = 'zh-CN'
+            self.dest = "zh-CN"
         elif lan == 1:
-            self.dest = 'en'
+            self.dest = "en"
         elif lan == 2:
-            self.dest = 'ja'
+            self.dest = "ja"
         elif lan == 3:
-            self.dest = 'ko'
+            self.dest = "ko"
         else:
-            self.dest = 'en'
+            self.dest = "en"
         print(self.dest)
- 
+
     def transTextToZhCN(self):
         text = self.originText.toPlainText()
         if text:
-            if self.paperMode.isChecked(): # if paper mode is true, line breaks will re replaced by blanks
-                text = re.sub(r'\n|\s+', ' ', text)
-                text = re.sub(r'', '', text)
+            if (
+                self.paperMode.isChecked()
+            ):  # if paper mode is true, line breaks will re replaced by blanks
+                text = re.sub(r"\n|\s+", " ", text)
+                text = re.sub(r"", "", text)
                 # add on 19/05/16
-                text = text.replace('', 'fi').replace('', 'ffi').replace('', 'ff').replace('', 'fl').replace('', 'th').replace('', 'ft').replace('', 'ft').replace('', 'tt')
+                text = (
+                    text.replace("", "fi")
+                    .replace("", "ffi")
+                    .replace("", "ff")
+                    .replace("", "fl")
+                    .replace("", "th")
+                    .replace("", "ft")
+                    .replace("", "ft")
+                    .replace("", "tt")
+                )
             self.originText.setPlainText(text)
             try:
                 # self.transText.setPlainText(trans_To_zh_CN(text))
-                self.t=GTranslator(self.dest, text)
+                self.t = GTranslator(self.dest, text)
                 self.t.start()
                 self.transText.setPlainText("")
                 self.transText.setPlaceholderText("...")
@@ -155,7 +185,9 @@ class MyWindow(QMainWindow, Ui_MWin):
         """
         if self.alwaysFront.isChecked():
             # print("Front", self.windowFlags())
-            self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint) # always top
+            self.setWindowFlags(
+                self.windowFlags() | Qt.WindowStaysOnTopHint
+            )  # always top
             self.show()
         else:
             # print("Back", self.win.windowFlags())
@@ -185,16 +217,27 @@ class MyWindow(QMainWindow, Ui_MWin):
         if text and self.isRealTimeTrans:
             content = str(text)
             # print(content)
-            content = content.replace('', 'fi').replace('', 'ffi').replace('', 'ff').replace('', 'fl').replace('', 'th').replace('', 'ft').replace('', 'ft').replace('', 'tt')
-            if self.paperMode.isChecked(): # if paper mode is true, line breaks will re replaced by blanks
-                content = re.sub(r'\n|\s+', ' ', content)
-                content = re.sub(r'', '', content)
+            content = (
+                content.replace("", "fi")
+                .replace("", "ffi")
+                .replace("", "ff")
+                .replace("", "fl")
+                .replace("", "th")
+                .replace("", "ft")
+                .replace("", "ft")
+                .replace("", "tt")
+            )
+            if (
+                self.paperMode.isChecked()
+            ):  # if paper mode is true, line breaks will re replaced by blanks
+                content = re.sub(r"\n|\s+", " ", content)
+                content = re.sub(r"", "", content)
             self.originText.setPlainText(content)
             self.transText.setPlainText(content)
             try:
                 # data = trans_To_zh_CN(content)
                 # self.transText.setPlainText(data)
-                self.t=GTranslator(self.dest, content)
+                self.t = GTranslator(self.dest, content)
                 self.t.start()
                 self.transText.setPlainText("")
                 self.transText.setPlaceholderText("...")
@@ -203,29 +246,31 @@ class MyWindow(QMainWindow, Ui_MWin):
                 self.transText.setPlainText("error!")
 
 
-class GTranslator(QThread): 
+class GTranslator(QThread):
     trigger = pyqtSignal()
+
     def __init__(self, dest, content):
         super().__init__()
         self.content = content
         self.dest = dest
-  
-    def run(self): 
+
+    def run(self):
         Data = []
         global GTransData
-        T = Translator(service_urls=['translate.google.cn'])
+        T = Translator(service_urls=["translate.google.cn"])
         # ts = T.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='zh-CN')
         try:
-        	ts = T.translate(self.content, dest=self.dest)
-	        if isinstance(ts.text, list):
-	            for i in ts:
-	                Data.append(i.text)
-	            GTransData = Data
-	        else:
-	            GTransData = ts.text
+            ts = T.translate(self.content, dest=self.dest)
+            if isinstance(ts.text, list):
+                for i in ts:
+                    Data.append(i.text)
+                GTransData = Data
+            else:
+                GTransData = ts.text
         except:
-        	GTransData = 'An error happended. Please retry...'
-        self.trigger.emit()         # emit signal once translati is finfished
+            GTransData = "An error happended. Please retry..."
+        self.trigger.emit()  # emit signal once translatation is finfished
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
